4bbdb7fc61cd0a1c2e40b28e6e41af0396cdc729
Udayan <161637160+mukherjeeUDayan@users.noreply.github.com>
2024-10-09 19:42:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
+++ Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
@@ -12,19 +12,24 @@ class Solution {
   public:
     int maxLen(vector<int>& arr, int n) {
         // Your code here
-        int maxi=0;
-        unordered_map<int,int>mp;
-        mp[0]=-1;
-        int sum=0;
-        for(int i=0; i<n; i++){
-            sum+=arr[i];
-            if(mp.find(sum)!=mp.end()){
-                maxi= max(maxi, i-mp[sum]);
-            }else{
-                mp[sum]=i;
-            }
+        
+    int maxi = 0;
+    unordered_map<int, int> mp;
+    mp[0] = -1;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        if (mp.find(sum) != mp.end())
+        {
+            maxi = max(maxi, i - mp[sum]);
         }
-        return maxi;
+        else
+        {
+            mp[sum] = i;
+        }
+    }
+    return maxi;
     }
 };
 
