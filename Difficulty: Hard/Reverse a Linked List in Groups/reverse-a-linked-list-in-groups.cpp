850347b128de99dfafaa03a2efb4354b38f91083
Udayan <161637160+mukherjeeUDayan@users.noreply.github.com>
2024-10-09 20:09:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Reverse a Linked List in Groups/reverse-a-linked-list-in-groups.cpp
@@ -0,0 +1,125 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+   // Redirecting input from file
+  freopen("/Users/debojyoti.mandal/expt/input.txt", "r", stdin);
+
+  // Redirecting output to file
+  freopen("/Users/debojyoti.mandal/expt/output.txt", "w", stdout);
+*/
+
+struct node {
+    int data;
+    struct node* next;
+
+    node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to print linked list */
+void printList(struct node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+/*
+  Reverse a linked list
+  The input list will have at least one element
+  Return the node which points to the head of the new LinkedList
+  Node is defined as
+    struct node
+    {
+        int data;
+        struct node* next;
+
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+
+    }*head;
+*/
+
+class Solution {
+  public:
+    struct node *reverseIt(struct node *head, int k) {
+        // Complete this method
+        if(head == nullptr){
+            return nullptr;
+        }
+        node* curr= head;
+        node* prev= nullptr;
+        node* fordw= nullptr;
+        int cnt=0;
+        while(cnt < k && curr!= nullptr){
+            fordw= curr->next;
+            curr->next= prev;
+            prev= curr;
+            curr= fordw;
+            cnt++;
+        }
+        if(fordw != nullptr){
+            head->next= reverseIt(fordw,k);
+        }
+        return prev;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void) {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        node* head = new node(data);
+        node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new node(data);
+            tail = tail->next;
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+
+        Solution ob;
+        head = ob.reverseIt(head, k);
+        printList(head);
+    }
+
+    return (0);
+}
+
+// } Driver Code Ends
