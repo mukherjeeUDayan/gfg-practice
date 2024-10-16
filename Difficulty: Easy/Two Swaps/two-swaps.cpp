af63fecdce0323b6836c1639652b0ecc70dcf192
Udayan <161637160+mukherjeeUDayan@users.noreply.github.com>
2024-10-16 22:52:57 +0530

    pushed two-swaps.cpp

---
+++ Difficulty: Easy/Two Swaps/two-swaps.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// 
+class Solution {
+  public:
+    bool checkSorted(vector<int> &arr) {
+        // code here.
+        int cnt=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            while(arr[i]!=arr[arr[i]-1])
+            {
+                swap(arr[i],arr[arr[i]-1]);
+                cnt++;
+            }
+        }
+        return cnt==2 or cnt==0;
+    }
+};
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        vector<int> arr;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution ob;
+        bool ans = ob.checkSorted(arr);
+        if (ans)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+}
