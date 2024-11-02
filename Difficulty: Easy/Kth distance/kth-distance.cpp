e5b9b2d0dfc5f02b38ae1b0dfa66f60fc558542d
Udayan <161637160+mukherjeeUDayan@users.noreply.github.com>
2024-11-02 15:36:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth distance/kth-distance.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+
+    bool checkDuplicatesWithinK(vector<int>& arr, int k) {
+        // your code
+    unordered_set<int> st;
+    
+    for (int i = 0; i < arr.size(); i++) {
+        if (st.count(arr[i])) {
+            return true;
+        }
+        st.insert(arr[i]);
+        
+        // Maintain the size of the set to be at most k
+        if (i >= k) {
+            st.erase(arr[i - k]); // Remove the element that is out of the window
+        }
+    }
+    
+    return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        bool res = obj.checkDuplicatesWithinK(arr, k);
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
