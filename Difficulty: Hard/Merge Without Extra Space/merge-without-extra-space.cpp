bf6fa0a7e082e92e26d2675fb9b82ac5a691ef91
Udayan <161637160+mukherjeeUDayan@users.noreply.github.com>
2024-12-11 00:33:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        // code here
+        int n = a.size();
+        int m = b.size();
+
+        // Start comparing from the end of the first array and the start of the second array
+        for (int i = n - 1, j = 0; i >= 0 && j < m; i--, j++) {
+            if (a[i] > b[j]) {
+                // Swap the elements
+                int temp = a[i];
+                a[i] = b[j];
+                b[j] = temp;
+            }
+        }
+
+        // Sort both arrays individually
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t; // Inputting the test cases
+
+    while (t--) {
+        vector<int> a, b;
+
+        // Reading the first array as a space-separated line
+        string arr1;
+        getline(cin >> ws, arr1); // Use ws to ignore any leading whitespace
+        stringstream ss1(arr1);
+        int num;
+        while (ss1 >> num) {
+            a.push_back(num);
+        }
+
+        // Reading the second array as a space-separated line
+        string arr2;
+        getline(cin, arr2);
+        stringstream ss2(arr2);
+        while (ss2 >> num) {
+            b.push_back(num);
+        }
+
+        Solution ob;
+        ob.mergeArrays(a, b);
+
+        // Output the merged result
+        for (int i = 0; i < a.size(); i++) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+        for (int i = 0; i < b.size(); i++) {
+            cout << b[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
