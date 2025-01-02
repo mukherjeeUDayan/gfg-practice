ed92c3c4576c01ff7cc8f7cce986825c6bf6af2c
Udayan <161637160+mukherjeeUDayan@users.noreply.github.com>
2025-01-02 21:58:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        // taking input using class Scanner
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            int res = new Solution().countSubarrays(arr, k);
+
+            System.out.print(res);
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public int countSubarrays(int arr[], int k) {
+        // code here
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        mp.put(0,1);
+        int sum=0 ,answ=0;
+        for(int val:arr){
+            sum+=val;
+            if(mp.containsKey(sum-k))
+                answ+=mp.get(sum-k);
+            if(mp.containsKey(sum))
+                mp.put(sum,mp.get(sum)+1);
+            else
+                mp.put(sum,1);
+        }
+        return answ;
+    }
+}
