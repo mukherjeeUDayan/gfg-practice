b50405e4ee2fc4458fadc694f9c7c2a2b9f1d002
Udayan <161637160+mukherjeeUDayan@users.noreply.github.com>
2025-03-16 17:20:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.py
@@ -0,0 +1,25 @@
+class Solution:
+	def minJumps(self, arr):
+	    # code here
+        cur, prv, step = 0, -1, 0
+        while cur < len(arr) - 1:
+            mx = max([prv+1+i+e for i,e in enumerate(arr[prv+1:cur+1])])
+            if mx>cur:
+                cur, prv, step = mx, cur, step+1
+            else:
+                return -1
+        return step
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        # n = int(input())
+        Arr = [int(x) for x in input().split()]
+        ob = Solution()
+        ans = ob.minJumps(Arr)
+        print(ans)
+        print("~")
+# } Driver Code Ends
