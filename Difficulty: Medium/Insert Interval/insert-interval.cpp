fcec88f3e55a36895fc7d97b1f78d4aac1d216ac
Udayan <161637160+mukherjeeUDayan@users.noreply.github.com>
2025-08-15 00:20:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Insert Interval/insert-interval.cpp
+++ Difficulty: Medium/Insert Interval/insert-interval.cpp
@@ -1,76 +1,31 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
-
 class Solution {
   public:
-    vector<vector<int>> insertInterval(vector<vector<int>> &intervals,
-                                       vector<int> &newInterval) {
+    vector<vector<int>> insertInterval(vector<vector<int>> &iv,
+                                       vector<int> &ni) {
         // code here
-        int n = intervals.size();
-        if(newInterval[1] <  intervals[0][0]){
-            intervals.insert(intervals.begin(), newInterval);
-            return intervals;
+        vector<vector<int>> ans;
+        int i = 0, n = iv.size();
+
+        // Step 1: add all intervals before ni
+        while (i < n && iv[i][1] < ni[0]) {
+            ans.push_back(iv[i]);
+            i++;
         }
-        else if(newInterval[0] > intervals[n-1][1]){
-            intervals.push_back(newInterval);
-            return intervals;
+
+        // Step 2: merge overlaps
+        while (i < n && iv[i][0] <= ni[1]) {
+            ni[0] = min(ni[0], iv[i][0]);
+            ni[1] = max(ni[1], iv[i][1]);
+            i++;
         }
-        else {
-            for(int i = 0 ; i < n ; i++){
-                if(newInterval[0] <= intervals[i][0]){
-                    intervals.insert(intervals.begin()+i,newInterval);
-                    break;
-                }
-            }
-            if(n == intervals.size()) intervals.push_back(newInterval);
-            int i = 1; 
-            while(i < intervals.size()){
-                if(intervals[i][0] > intervals[i-1][1])
-                 i++;
-                else {
-                    intervals[i-1][1] = max(intervals[i-1][1], intervals[i][1]);
-                    intervals.erase(intervals.begin()+i);
-                }
-            }
-            
+        ans.push_back(ni);
+
+        // Step 3: add remaining
+        while (i < n) {
+            ans.push_back(iv[i]);
+            i++;
         }
-        return intervals ;
+
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        vector<vector<int>> intervals(N, vector<int>(2));
-        for (int i = 0; i < N; i++) {
-            cin >> intervals[i][0] >> intervals[i][1];
-        }
-        vector<int> newInterval(2);
-        cin >> newInterval[0] >> newInterval[1];
-
-        Solution obj;
-        vector<vector<int>> ans = obj.insertInterval(intervals, newInterval);
-        cout << "[";
-        for (int i = 0; i < ans.size(); i++) {
-            cout << "[" << ans[i][0] << ',' << ans[i][1] << ']';
-            if (i != (ans.size() - 1))
-                cout << ",";
-        }
-        cout << "]" << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
