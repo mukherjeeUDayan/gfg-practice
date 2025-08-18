6895eeb1f51b92c9d07d5594c14971040cfdfdbb
Udayan <161637160+mukherjeeUDayan@users.noreply.github.com>
2025-08-18 21:54:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.py
+++ Difficulty: Medium/Find H-Index/find-hindex.py
@@ -1,29 +1,12 @@
-#User function Template for python3
 class Solution:
-    # Function to find hIndex
     def hIndex(self, citations):
         #code here
-        citations.sort(reverse=True)
-        for i, c in enumerate(citations):
-            if c <= i:
-                return i
-        return len(citations)
-
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-
-# Main
-if __name__ == '__main__':
-    t = int(input())
-    while t > 0:
-        t -= 1
-        arr = [int(x) for x in input().strip().split()]
-
-        ob = Solution()
-        result = ob.hIndex(arr)
-
-        print(result)
-        print("~")
-
-# } Driver Code Ends
+        import heapq
+        heapq.heapify(citations)
+        h=len(citations)
+        while citations:
+            if citations[0]>=h:
+                return h
+            heapq.heappop(citations)
+            h-=1
+        return 0
