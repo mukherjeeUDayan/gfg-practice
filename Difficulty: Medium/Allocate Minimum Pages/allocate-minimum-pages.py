a2d578f394fa78e64922cd8ccb913010a0dae315
Udayan <161637160+mukherjeeUDayan@users.noreply.github.com>
2025-08-23 23:19:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
@@ -1,48 +1,24 @@
 class Solution:
-    
-    #Function to find minimum number of pages.
-    def findPages(self, arr, k):
-        #code here
-        n=len(arr)
-        if n<k:
-            return -1
-        low, high= max(arr), sum(arr)
-        while low < high:
-            mid=(low+high)//2
-            if Solution.isPossible(arr, k, mid):
-                high=mid
-            else:
-                low =mid +1
-        return low
-    def isPossible(arr, k, maxPages):
-        students, currentSum = 1, 0
+    def isPossible(self,arr,val,k):
+        count,curr=1,0
         for pages in arr:
-            if currentSum + pages > maxPages:
-                students += 1
-                currentSum = pages
-                if students > k:
-                    return False
-            else:
-                currentSum += pages
-
+            curr+=pages
+            if curr>val:
+                curr=pages
+                count+=1
+            if count>k or pages>val:
+                return False
         return True
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import bisect
-#Main
-if __name__ == '__main__':
-    t = int(input())
-    while t:
-        t -= 1
-        A = [int(x) for x in input().strip().split()]
-        nd = [int(x) for x in input().strip().split()]
-        D = nd[0]
-        ob = Solution()
-        ans = ob.findPages(A, D)
-        print(ans)
-        print("~")
-
-# } Driver Code Ends
+    
+    def findPages(self, arr, k):
+        if len(arr)<k:
+            return -1
+        l,h,ans=0,sum(arr),-1
+        while l<=h:
+            mid=(l+h)//2
+            if self.isPossible(arr,mid,k):
+                ans=mid
+                h=mid-1
+            else:
+                l=mid+1
+        return ans
