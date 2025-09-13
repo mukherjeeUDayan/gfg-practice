a08cfa35d5b878263ef01dd32b99418463f03d49
Udayan <161637160+mukherjeeUDayan@users.noreply.github.com>
2025-09-14 00:01:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.py
+++ Difficulty: Medium/Gas Station/gas-station.py
@@ -1,6 +1,6 @@
 class Solution:
     def startStation(self, gas, cost):
-        # Your code here
+        #  code here
         if sum(gas) < sum(cost):
             return -1
         start_index = 0
@@ -12,23 +12,3 @@ class Solution:
                 start_index = i + 1
                 current_gas = 0
         return start_index
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import io
-import sys
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        gas = list(map(int, input().strip().split()))
-        cost = list(map(int, input().strip().split()))
-        ob = Solution()
-        ans = ob.startStation(gas, cost)
-        print(ans)
-        print("~")
-
-# } Driver Code Ends
